f3d11c3f291debf2d2127253d44c650e24040b54
prony5 <prony5@yandex.ru>
2024-04-11 11:45:35 +0500

    Small fix

--- Runtime/Tx/Tf/TfLink.cs
+++ Runtime/Tx/Tf/TfLink.cs
@@ -5,7 +5,7 @@ namespace ProBridge.Tx.Tf
     [AddComponentMenu("ProBridge/Tf/Link")]
     public class TfLink : MonoBehaviour
     {
-        public string frame_id;
-        public TfLink[] children;
+        public string frame_id = "";
+        public TfLink[] children = new TfLink[0];
     }
 }
