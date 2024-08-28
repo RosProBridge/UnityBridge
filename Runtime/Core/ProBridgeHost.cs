92cffb36144ee72dbae491816b2ec1fe89aaae47
Sherif Nafee <shico.m.nafee@gmail.com>
2024-08-28 14:49:33 +0300

    Add connection detection to host

--- Runtime/Core/ProBridgeHost.cs
+++ Runtime/Core/ProBridgeHost.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Threading;
 using NetMQ;
 using UnityEngine;
 using NetMQ.Sockets;
+using NetMQ.Monitoring;
 
 namespace ProBridge
 {
@@ -13,23 +15,46 @@ namespace ProBridge
 
         [HideInInspector] public PublisherSocket publisher;
 
+        public event EventHandler onSubscriberConnect;
+        private NetMQMonitor monitor;
+
+        private Thread monitoringThread;
+        private bool shouldStopMonitoring = false;
+
         private void OnEnable()
         {
             AsyncIO.ForceDotNet.Force();
             publisher = new PublisherSocket();
             publisher.Bind($"tcp://{addr}:{port}");
             publisher.Options.Linger = new TimeSpan(0, 0, 1);
+
+            shouldStopMonitoring = false;
+            monitoringThread = new Thread(StartMonitoring);
+            monitoringThread.Start();
+        }
+
+        private void StartMonitoring()
+        {
+            monitor = new NetMQMonitor(publisher, $"inproc://monitor-{addr}:{port}", SocketEvents.All);
+            monitor.Accepted += (s, e) => onSubscriberConnect?.Invoke(this, EventArgs.Empty);
+
+            monitor.Start();
         }
 
         private void OnDisable()
         {
             publisher.Close();
             publisher?.Dispose();
+
+            monitor.Stop();
+            monitor?.Dispose();
+            if (monitoringThread != null && monitoringThread.Join(1000))
+                monitoringThread?.Abort();
         }
 
         private void OnDestroy()
         {
-            NetMQConfig.Cleanup(false);  // Must be here to work more than once, and false to not block when there are unprocessed messages.
+            NetMQConfig.Cleanup(false); // Must be here to work more than once, and false to not block when there are unprocessed messages.
         }
     }
 }
