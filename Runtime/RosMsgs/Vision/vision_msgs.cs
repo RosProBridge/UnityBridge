107b058fbba5dae28c1069281d62245120d92f40
Siba <s.issa@innopolis.university>
2024-09-12 13:49:42 +0300

    Added summeries to the msgs

--- Runtime/RosMsgs/Vision/vision_msgs.cs
+++ Runtime/RosMsgs/Vision/vision_msgs.cs
@@ -14,7 +14,14 @@ namespace vision_msgs
                 return "vision_msgs.msg.BoundingBox3D";
             }
 
+            /// <summary>
+            /// The 3D position and orientation of the bounding box center.
+            /// </summary>
             public Pose center;
+
+            /// <summary>
+            /// The size of the bounding box, in meters, surrounding the object's center pose.
+            /// </summary>
             public Vector3 size;
         }
 
@@ -25,8 +32,23 @@ namespace vision_msgs
                 return "vision_msgs.msg.ObjectHypothesisWithPose";
             }
 
+            /// <summary>
+            /// The unique numeric ID of object detected.
+            /// </summary>
             public long id;
+
+            /// <summary>
+            /// The probability or confidence value of the detected object. By convention,
+            /// this value should lie in the range [0-1].
+            /// </summary>
             public double score;
+
+            /// <summary>
+            /// The 6D pose of the object hypothesis. This pose should be
+            /// defined as the pose of some fixed reference point on the object, such a
+            /// the geometric center of the bounding box or the center of mass of the
+            /// object.
+            /// </summary>
             public PoseWithCovariance pose;
 
         }
@@ -40,8 +62,22 @@ namespace vision_msgs
 
             public Header header { get; set; } = new Header();
 
+            /// <summary>
+            /// Class probabilities. Does not have to include hypotheses for all possible
+            /// object ids, the scores for any ids not listed are assumed to be 0.
+            /// </summary>
             public ObjectHypothesisWithPose[] results;
+
+            /// <summary>
+            /// 3D bounding box surrounding the object.
+            /// </summary>
             public BoundingBox3D bbox;
+
+            /// <summary>
+            /// The 3D data that generated these results (i.e. region proposal cropped out of
+            /// the image). This information is not required for all detectors, so it may
+            /// be empty.
+            /// </summary>
             public PointCloud2 source_cloud;
 
         }
@@ -55,6 +91,10 @@ namespace vision_msgs
 
             public Header header { get; set; } = new Header();
 
+            /// <summary>
+            /// A list of the detected proposals. A multi-proposal detector might generate
+            /// this list with many candidate detections generated from a single input.
+            /// </summary>
             public Detection3D[] detections;
         }
     }
