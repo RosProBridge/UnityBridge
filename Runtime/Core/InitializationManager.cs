386f3aa9e802f689c973c2380365d60322c54f96
Sherif Nafee <shico.m.nafee@gmail.com>
2024-10-03 15:17:19 +0300

    Update InitializationManager.cs

--- Runtime/Core/InitializationManager.cs
+++ Runtime/Core/InitializationManager.cs
@@ -1,29 +1,68 @@
 ﻿using System;
-using NetMQ;
-using NetMQ.Monitoring;
 using NetMQ.Sockets;
-using ProBridge.Tx;
 using ProBridge.Tx.Tf;
 using UnityEngine;
 
 
 namespace ProBridge
 {
-    
     [DefaultExecutionOrder(-10000)]
     public class InitializationManager : ProBridgeSingletone<InitializationManager>
     {
+        [SerializeField] private bool _useConfigFile = false;
+
         private ProBridgeServer _server;
         private ProBridgeHost[] _hosts;
         private TfSender _tfSender;
 
-        private void Awake()
+        private void OnEnable()
         {
             _server = FindObjectOfType<ProBridgeServer>();
             _hosts = FindObjectsOfType<ProBridgeHost>();
             _tfSender = FindObjectOfType<TfSender>();
-            
-            
+
+            if(_useConfigFile)
+
+            {
+                ParamsReader.Load();
+
+                var config = ParamsReader.Value;
+
+                // Get config for hosts
+                foreach (var host in _hosts)
+                {
+                    var proBridgeHost = config.GetObjectByTypeAndName("ProBridgeHost", host.gameObject.name);
+
+                    if (proBridgeHost == null)
+                    {
+                        Debug.LogWarning("ProBridgeHost configuration not found. Using Default values.");
+                        continue;
+                    }
+
+                    var port = proBridgeHost.GetField("port");
+                    var addr = proBridgeHost.GetField("addr");
+                    host.addr = addr;
+                    host.port = Convert.ToInt32(port);
+                }
+                
+                // Get config for server
+                var proBridgeServer = config.GetObjectByType("ProBridgeServer");
+
+                if (proBridgeServer != null)
+                {
+                    var port = proBridgeServer.GetField("port");
+                    var addr = proBridgeServer.GetField("addr");
+
+                    _server.port = Convert.ToInt32(port);
+                    _server.ip = addr;
+                }
+                else
+                {
+                    Debug.LogWarning("ProBridgeServer configuration not found. Using Default values.");
+                }
+            }
+
+
             // Init hosts sockets
             AsyncIO.ForceDotNet.Force();
             foreach (var host in _hosts)
@@ -32,24 +71,24 @@ namespace ProBridge
                 host.pushSocket.Connect($"tcp://{host.addr}:{host.port}");
                 host.pushSocket.Options.Linger = new TimeSpan(0, 0, 1);
             }
-            
-            
+
+
             // Init server
             try
             {
+                _server._initTime = DateTime.Now.Ticks;
                 _server.Bridge = new ProBridge(_server.port, _server.ip);
                 _server.Bridge.onMessageHandler += _server.OnMsg;
-                _server.Bridge.onDebugHandler += _server.OnLogMessage;
             }
             catch (Exception ex)
             {
                 _server.Bridge = null;
                 Debug.Log(ex);
             }
-            
-            
+
+
             // Init tf sender
-            if(_tfSender != null)
+            if (_tfSender != null)
             {
                 _tfSender.Bridge = _server.Bridge;
                 if (_tfSender.Bridge == null)
@@ -67,7 +106,7 @@ namespace ProBridge
             {
                 Debug.LogWarning("No TFSender found in scene.");
             }
-            
+
             // Init host monitors
             foreach (var host in _hosts)
             {
