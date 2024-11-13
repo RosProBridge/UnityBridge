61bde60ed3a78f678497e1b84dd21cef66424f95
pakhomovda <pakhomovda@a-r-s.dev>
2024-11-13 15:53:33 +0300

    add VfrHud message

---
+++ Runtime/RosMsgs/mavros_msgs/VfrHud.cs
@@ -0,0 +1,53 @@
+using std_msgs;
+using std_msgs.msg;
+
+namespace mavros_msgs
+{
+    namespace msg
+    {
+        /// <summary>
+        /// Metrics typically displayed on a HUD for fixed wing aircraft
+        /// </summary>
+        [System.Serializable]
+        public class VfrHud : IRosMsg, IStamped
+        {
+            public string GetRosType()
+            {
+                return "mavros_msgs.msg.VfrHud";
+            }
+
+            public std_msgs.msg.Header header { get; set; } = new std_msgs.msg.Header();
+
+            /// <summary>
+            /// m/s
+            /// </summary>
+            public float airspeed;
+
+            /// <summary>
+            /// m/s
+            /// </summary>
+            public float groundspeed;
+
+            /// <summary>
+            /// degrees 0..360
+            /// </summary>
+            public short heading;
+
+            /// <summary>
+            /// normalized to 0.0..1.0
+            /// </summary>
+            public float throttle;
+
+            /// <summary>
+            /// MSL
+            /// </summary>
+            public float altitude;
+
+            /// <summary>
+            /// current climb rate m/s
+            /// </summary>
+            public float climb;
+
+        }
+    }
+}
