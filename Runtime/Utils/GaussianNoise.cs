c74a82893bc43986d4260501e98e93aef2a8db33
Sherif Nafee <shico.m.nafee@gmail.com>
2024-12-13 12:06:26 +0300

    Add float override to gaussian noise

--- Runtime/Utils/GaussianNoise.cs
+++ Runtime/Utils/GaussianNoise.cs
@@ -16,5 +16,18 @@ namespace Utils
             double randStdNormal = Math.Sqrt(-2.0d * Math.Log(u1)) * Math.Sin(2.0d * Math.PI * u2);
             return randStdNormal * sigma;
         }
+
+        /// <summary>
+        /// Generates Gaussian noise using the Box-Muller transform.
+        /// </summary>
+        /// <param name="sigma">The standard deviation of the Gaussian distribution.</param>
+        /// <returns>A float representing the Gaussian noise with the given standard deviation.</returns>
+        public static float Generate(float sigma = 1.0f)
+        {
+            float u1 = UnityEngine.Random.value;
+            float u2 = UnityEngine.Random.value;
+            float randStdNormal = (float)(Math.Sqrt(-2.0f * Math.Log(u1)) * Math.Sin(2.0f * Math.PI * u2));
+            return randStdNormal * sigma;
+        }
     }
 }
