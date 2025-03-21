0d3d580b87a27be3020d7d817e449c17e8636eed
Sherif Nafee <shico.m.nafee@gmail.com>
2025-03-21 14:18:44 +0300

    Add ability to add tx stamps manually

--- Runtime/Tx/ProBridgeTxStamped.cs
+++ Runtime/Tx/ProBridgeTxStamped.cs
@@ -5,11 +5,13 @@ namespace ProBridge.Tx
     public abstract class ProBridgeTxStamped<T> : ProBridgeTx<T> where T : std_msgs.IRosMsg, std_msgs.msg.IStamped, new()
     {
         public string frame_id = "";
+        
+        protected bool autoAddStamp = true;
 
         protected override ProBridge.Msg GetMsg(TimeSpan ts)
         {
             data.header.frame_id = frame_id;
-            data.header.stamp = ts;
+            if(autoAddStamp) data.header.stamp = ts;
 
             return base.GetMsg(ts);
         }
